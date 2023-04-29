66362f4875315ce84cc73dfab73f8410d9732174
VladimirDan <vovaoil2003@gmail.com>
2023-04-29 23:44:40 +0300

    change the base image to slim-bullseye + add matrix by numpy

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix1 = numpy.random.rand(10, 10)
+    matrix2 = numpy.random.rand(10, 10)
+    result = numpy.matmul(matrix1, matrix2)
+    return {
+        'matrix1': matrix1.tolist(),
+        'matrix2': matrix2.tolist(),
+        'result': result.tolist()
+    }
